32e05abdec057c5ac0d6f6d0e296285c7d9605e1
Pablo Cubico <pablocuadrado@gmail.com>
2016-02-17 16:43:37 -0300

    Loading messages and setting browser's locale by default.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,18 @@ import { browserHistory, Router, Route } from 'react-router';
 import { Provider } from 'react-redux';
 // Redux Devtools
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
 // React-Intl 
+
 import {IntlProvider} from 'react-intl';
 require('./supportedLocales.js');
+import messages from './messages';
+
+var locale = navigator.language.split('-');
+locale = locale[1] ? `${locale[0]}-${locale[1].toUpperCase()}` : navigator.language;
+
+var strings = messages[locale] ? messages[locale] : messages['en-US'];
+strings = Object.assign(messages['en-US'], strings);
 
 import configureStore from './store';
 
@@ -38,7 +47,7 @@ class Root extends React.Component {
 
     return (
       <div>
-        <IntlProvider>
+        <IntlProvider messages={strings} locale={locale}>
           <Provider store={store}>
             <Router history={browserHistory}>
               <Route path="/" component={Dashboard}/>
