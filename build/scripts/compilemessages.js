60907eeef12b186b55da2c58871a4d190bf20a3c
Pablo Cubico <pablocuadrado@gmail.com>
2016-02-17 13:22:27 -0300

    Adding some comments, deleting unused stuff.

--- build/scripts/compilemessages.js
+++ build/scripts/compilemessages.js
@@ -6,8 +6,9 @@ import {sync as mkdirpSync} from 'mkdirp';
 const DEFAULT_MESSAGES_PATTERN = './build/messages/**/*.json';
 const LANG_DIR = './build/lang/';
 
-// A file that contains all the addLocaleData calls
-//require('../../src/supportedLocales.js');
+// TODO: determine required languages using a config file or
+// a file that contains all the addLocaleData calls as:
+// require('../../src/supportedLocales.js');
 
 // Collect default messages from components
 let defaultMessages = globSync(DEFAULT_MESSAGES_PATTERN)
@@ -26,20 +27,9 @@ let defaultMessages = globSync(DEFAULT_MESSAGES_PATTERN)
     }, {});
 
 // Collect existing messages (if any)
-/*let existingMessages = globSync(LANG_DIR + '*.json')
-    .map((filename) => fs.readFileSync(filename, 'utf8'))
-    .map((file) => JSON.parse(file))
-    .reduce((collection, descriptors) => {
-        descriptors.forEach(({id, existingMessage}) => {
-            collection[id] = existingMessage;
-        });
+// let existingMessages = globSync(LANG_DIR + '*.json')...
 
-        return collection;
-    }, {});*/
-
-
-// Property shorthand, see: https://lodash.com/docs
-//_.intersectionBy(defaultMessages, existingMessages, 'id');
+// TODO: compare default and existing, sync them
 
 mkdirpSync(LANG_DIR);
 fs.writeFileSync(LANG_DIR + 'en-US.json', JSON.stringify(defaultMessages, null, 2));
