3dadccb6dd8a41c32797494e2afcb72572139cf0
Pablo Cubico <pablocuadrado@gmail.com>
2016-02-24 16:50:29 -0300

    Added a tagger component in UserDetail.

--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -4,7 +4,7 @@ import Radium from 'radium';
 import settings from '../settings';
 import {connect} from 'react-redux';
 
-import {clearUserDetailComments} from '../actions';
+import {fetchAllTags, clearUserDetailComments} from '../actions';
 
 import Avatar from './Avatar';
 import Tab from './tabs/Tab';
@@ -13,6 +13,7 @@ import Stats from './stats/Stats';
 import Stat from './stats/Stat';
 import Card from './cards/Card';
 import Heading from './Heading';
+import Tagger from './forms/Tagger';
 
 import CommentDetailList from './CommentDetailList';
 
@@ -20,7 +21,21 @@ import CommentDetailList from './CommentDetailList';
 @Radium
 export default class UserDetail extends React.Component {
 
+  componentWillMount() {
+    this.props.dispatch(fetchAllTags());
+  }
+
+  componentWillUpdate() {
+    this.tags = [];
+  }
+
   render() {
+
+    var tagger = this.props.tags ?
+      <div style={ styles.tags }>
+        <Tagger tagList={ this.props.tags } tags={ this.tags } freeForm={ false } type="user" id={ this.props.selectedUser._id } />
+      </div>
+    : null;
 
     return (
       <Card style={[styles.base, this.props.style]}>
@@ -32,6 +47,7 @@ export default class UserDetail extends React.Component {
             <Stat term="Last Login" description={new Date().toISOString()} />
             <Stat term="Member Since" description={new Date().toISOString()} />
             <Stat term="Warnings" description="0" />
+            { tagger }
           </Stats>
         </div>
         <Tabs initialSelectedIndex={0} style={styles.tabs}>
@@ -72,5 +88,9 @@ const styles = {
   },
   tabs: {
     clear: 'both'
+  },
+  tags: {
+    clear: 'both',
+    paddingTop: '20px'
   }
 };
