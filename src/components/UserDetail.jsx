c00d9e0e1b8a0ed67caebea40544bb70ebfeb434
Pablo Cubico <pablocuadrado@gmail.com>
2016-02-25 19:05:55 -0300

    Buggy state, commiting to merge the parent branch.

--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -4,7 +4,7 @@ import Radium from 'radium';
 import settings from '../settings';
 import {connect} from 'react-redux';
 
-import {fetchAllTags, clearUserDetailComments} from '../actions';
+import {fetchAllTagsUserDetail, upsertUser} from '../actions';
 
 import Avatar from './Avatar';
 import Tab from './tabs/Tab';
@@ -22,18 +22,33 @@ import CommentDetailList from './CommentDetailList';
 export default class UserDetail extends React.Component {
 
   componentWillMount() {
-    this.props.dispatch(fetchAllTags());
+    this.props.dispatch(fetchAllTagsUserDetail());
   }
 
   componentWillUpdate() {
     this.tags = [];
   }
 
+  onTagsChange(tags) {
+    if (this.props.selectedUser && this.props.selectedUser._id) {
+      var preparedUser = {};
+      preparedUser['id'] = this.props.selectedUser._id;
+      preparedUser['name'] = this.props.selectedUser.user_name;
+      preparedUser['avatar'] = this.props.selectedUser.avatar;
+      preparedUser['status'] = this.props.selectedUser.status;
+      preparedUser['tags'] = [];
+      for(var i in tags) {
+        preparedUser['tags'].push(tags[i].text);
+      }
+      this.props.dispatch(upsertUser(preparedUser));
+    }
+  }
+
   render() {
 
     var tagger = this.props.tags ?
       <div style={ styles.tags }>
-        <Tagger tagList={ this.props.tags } tags={ this.tags } freeForm={ false } type="user" id={ this.props.selectedUser._id } />
+        <Tagger onChange={ this.onTagsChange.bind(this) } tagList={ this.props.tags } tags={ this.tags } freeForm={ false } type="user" id={ this.props.selectedUser && this.props.selectedUser._id ? this.props.selectedUser._id : 1 } />
       </div>
     : null;
 
